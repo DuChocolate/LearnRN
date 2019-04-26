828890c907727a71c8773d17168aaa92d58c6f2a
DuChocolate <305096012@qq.com>
2019-04-26 18:56:48 +0800

    注册

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,35 +15,75 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
-type Props = {};
-export default class App extends Component<Props> {
+let widthOfMargin = Dimensions.get('window').width * 0.05;
+// type Props = {};
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      inputedNum: '',
+      inputedPW: ''
+    }
+    this.updatePW = this.updatePW.bind(this);
+  }
+  updateNum = (newText) => {
+    this.setState({
+      inputedNum: newText
+    });
+  }
+  updatePW = (newText) => {
+    this.setState({
+      inputedPW: newText
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!你好啊，我来了</Text>
+        <TextInput style={styles.textInputStyle} placeholder={'请输入手机号'} onChangeText={(newText) => this.updateNum(newText)}/>
+        <Text style={styles.textPromptStyle}>您输入的手机号：{this.state.inputedNum}</Text>
+        <TextInput style={styles.textInputStyle} placeholder={'请输入密码'} secureTextEntry={true} onChangeText={this.updatePW}/>
+        <Text style={styles.bigTextPrompt}>确定</Text>
+
+
+
+        {/* <Text style={styles.welcome}>Welcome to React Native!,你好34</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textInputStyle: {
+    margin: widthOfMargin,
+    backgroundColor: 'gray',
+    fontSize: 20
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textPromptStyle: {
+    margin: widthOfMargin,
+    fontSize: 20
   },
+  bigTextPrompt: {
+    margin: widthOfMargin,
+    backgroundColor: 'gray',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30
+  }
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
 });
