5ba70ecf33a6f6c70ae5c49199e57494b4d00a87
DuChocolate <305096012@qq.com>
2019-04-29 19:02:02 +0800

    test

--- App.js
+++ App.js
@@ -7,57 +7,58 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, BackHandler, StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
-import {Navigator} from 'react-native-deprecated-custom-components';
-import LoginLeaf from './components/LoginLeaf';
-import WaitingLeaf from './components/WaitingLeaf';
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-let widthOfMargin = Dimensions.get('window').width * 0.05;
-// type Props = {};
+import {StyleSheet, Text, View} from 'react-native';
 export default class App extends Component {
   constructor(props){
     super(props);
-    this.renderScene = this.renderScene.bind(this);
-    this.configureScene = this.configureScene.bind(this);
-    this.handleBackAndroid = this.handleBackAndroid.bind(this);
-  }
-  componentDidMount() {
-    if(Platform.OS === 'android'){
-      BackHandler.addEventListener('hardwareBackPress', this.handleBackAndroid);
-    }
-  }
-  componentWillUnmount() {
-    if(Platform.OS === 'android'){
-      BackHandler.removeEventListener('hardwareBackPress', this.handleBackAndroid);
-    }
-  }
-  configureScene = (route) => {
-    return Navigator.SceneConfigs.FadeAndroid;
-  }
-  renderScene = (router, navigator) => {
-    this.navigator = navigator;
-    switch(router.name){
-      case 'login':
-        return <LoginLeaf navigator={navigator}/>;
-      case 'waiting':
-        return <WaitingLeaf phoneNumber={router.phoneNumber} userPW={router.userPW} navigator={navigator}/>;
-    }
-  }
-  handleBackAndroid = () =>{
-    if(this.navigator.getCurrentRoutes().length > 1){
-      this.navigator.pop();
-      return true;
-    }
-    return false;
   }
   render() {
     return (
-      <Navigator initialRoute={{name: 'login'}} configureScene={this.configureScene} renderScene={this.renderScene}/>
+      <View style={style.container}>
+        <View style={style.firstRow}>
+          <View style={styles.test1} />
+          <View style={styles.test2} />
+          <View style={styles.test3} />
+        </View>
+        <View style={styles.testPosition} />
+      </View>
     );
   }
 }
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  firstRow: {
+    height: 40,
+    top: 40,
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  test1: {
+    width: 68,
+    height: 24,
+    backgroundColor: 'white',
+  },
+  test2: {
+    width: 40,
+    height: 24,
+    backgroundColor: 'white',
+  },
+  test3: {
+    width: 100,
+    height: 24,
+    backgroundColor: 'white',
+  },
+  testPosition: {
+    backgroundColor: 'grey',
+    height: 60,
+    width: 60,
+    position: 'absolute',
+    top: 150,
+    right: 50
+  }
+})
